81f00cea010eb77c995d0ba942b89145571187aa
xgqfrms <xgqfrms@outlook.com>
2020-08-18 22:10:26 +0800

    cppt

---
+++ test/cppt.cpp
@@ -0,0 +1,28 @@
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2020-07-0
+ * @modified
+ *
+ * @description
+ * @difficulty Easy Medium Hard
+ * @complexity O(n)
+ * @augments
+ * @example
+ * @link
+ * @solutions
+ *
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello, World!\n");
+  // do something...
+  return 0;
+}
+
+
