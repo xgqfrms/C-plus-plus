e6ea3564029383deed9b1677d8a97c81ee2ecd02
xgqfrms <xgqfrms@outlook.com>
2023-01-20 04:18:26 +0800

    Create cout-line-break.cpp

---
+++ src/cout-line-break.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // printf 
+    printf("printf with \\n \n");
+    // \n
+    cout << "Hello world! \n";
+    // endl
+    cout << "Hello world!" << endl;
+    // "\n"
+    cout << "\"\\n\" === endl " << "\n";
+    return 0;
+}
+
+/*
+
+https://www.sololearn.com/learning/1051/1604/2734/1
+
+https://www.sololearn.com/compiler-playground/cDJig66Q0hd0
+
+*/
