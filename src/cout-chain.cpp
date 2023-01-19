cb5413c4c98c0d895817b976e76178dd675fa467
xgqfrms <xgqfrms@outlook.com>
2023-01-20 04:39:12 +0800

    Create cout-chain.cpp

---
+++ src/cout-chain.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int sum = 1 + 2;
+  // cout 链式调用
+  cout << "sum is equal to " << sum << endl;
+  // 等价于
+  cout << "sum is equal to "
+  cout << sum << endl;
+  return 0;
+}
+
+
+
