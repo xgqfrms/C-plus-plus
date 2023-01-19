0c7c2d2de7ce398e9683e4772fde8032529c613e
xgqfrms <xgqfrms@outlook.com>
2023-01-20 04:29:22 +0800

    Create cin.cpp

---
+++ src/cin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    // your code goes here
+    cin >> a;
+    cin >> b;
+    int sum = a + b;
+    cout << sum;
+    // cout << sum << endl;
+    return 0;
+}
