04a56d8a20223b5ab757624df8a3e041144346d9
xgqfrms <xgqfrms@outlook.com>
2023-01-21 01:46:41 +0800

    Create if-else.cpp

---
+++ src/if-else.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  double points;
+  cin >> points;
+  //your code goes here
+  if(points < 90){
+    cout << "fail" << endl;
+  } else {
+    cout << "pass" << endl;
+  }
+  return 0;
+}
