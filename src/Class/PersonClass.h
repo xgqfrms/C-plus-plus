637ee92321de0b3761f4ae65bfba14e69019db14
xgqfrms <xgqfrms@outlook.com>
2023-01-23 17:59:22 +0800

    Update PersonClass.h

--- src/Class/PersonClass.h
+++ src/Class/PersonClass.h
@@ -14,3 +14,10 @@ class PersonClass
 #endif
 // PERSON_CLASS_H
 
+/*
+
+The header file (.h) holds the function declarations (prototypes) and variable declarations.
+
+头文件 (.h) 包含函数声明（原型）和变量声明。
+
+*/
